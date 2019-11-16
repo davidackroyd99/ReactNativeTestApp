fd638029ceb8e3c6fdd330871859c21393d46420
David Ackroyd <davidackroydmusic@gmail.com>
2019-11-16 14:10:16 +0000

    added navigation

--- App.tsx
+++ App.tsx
@@ -1,63 +1,12 @@
-import React from 'react';
-import { Text, View, Dimensions, ScrollView } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-import { styles } from './Styles';
+import { Home } from './Home';
 
+const MainNavigator = createStackNavigator({
+  Home: {screen: Home},
+});
 
+const App = createAppContainer(MainNavigator);
 
-class Movie {
-	title: String;
-	year: Number;
-}
-
-export default class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			error: null,
-			isLoaded: false,
-			movies: []
-		};
-	}
-
-	componentDidMount() {
-		fetch("https://facebook.github.io/react-native/movies.json")
-			.then(res => res.json())
-			.then(
-			(result) => {
-				this.setState({
-					isLoaded: true,
-					movies: result.movies
-				});
-			})
-			.catch((error) => {
-				alert("error");
-				this.setState({
-					isLoaded: false,
-					error
-				});
-			});
-	}
-
-	render() {
-		return <View style={styles.container}>
-			<View style={styles.header}>
-				<Text style={styles.headerText}>My Movies</Text>
-			</View>
-			<View style={styles.body}>
-				<ScrollView horizontal={true} showsHorizontalScrollIndicator={false} pagingEnabled={true}>
-					{this.state.movies.map(movie => (
-						<View style={styles.scrollBox}>
-							<View style={[styles.imageDummy, styles.imageRed]} />
-							<Text>{movie.title}</Text>
-						</View>
-					))}
-				</ScrollView>
-			</View>
-			<View style={styles.footer}>
-				<Text>This was made with React Native.</Text>
-			</View>
-		</View>;
-	}
-}
-
+export default App;
---
+++ Home.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Text, View, Dimensions, ScrollView } from 'react-native';
+
+
+import { styles } from './Styles';
+
+
+export class Home extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			error: null,
+			isLoaded: false,
+			movies: []
+		};
+	}
+
+	componentDidMount() {
+		fetch("https://facebook.github.io/react-native/movies.json")
+			.then(res => res.json())
+			.then(
+			(result) => {
+				this.setState({
+					isLoaded: true,
+					movies: result.movies
+				});
+			})
+			.catch((error) => {
+				alert("error");
+				this.setState({
+					isLoaded: false,
+					error
+				});
+			});
+	}
+
+	render() {
+		return <View style={styles.container}>
+			<View style={styles.header}>
+				<Text style={styles.headerText}>My Movies</Text>
+			</View>
+			<View style={styles.body}>
+				<ScrollView horizontal={true} showsHorizontalScrollIndicator={false} pagingEnabled={true}>
+					{this.state.movies.map(movie => (
+						<View style={styles.scrollBox} key={movie.id}>
+							<View style={[styles.imageDummy, styles.imageRed]} />
+							<Text>{movie.title}</Text>
+						</View>
+					))}
+				</ScrollView>
+			</View>
+			<View style={styles.footer}>
+				<Text>This was made with React Native.</Text>
+			</View>
+		</View>;
+	}
+}
